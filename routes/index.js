c494fade39de357e3b583abed5f5252a0296d21a
MAC <mac@MacBook-Air.local>
2016-10-26 03:41:53 +0200

    added interval buying.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var fut = require('fut-api');
 var router = express.Router();
 var futService = require('../services/futservice');
 
-router.get('/', function(req, res){
+router.get('/', function (req, res) {
   // futService.requestLogin();
 
   res.render('index', {
@@ -11,64 +11,85 @@ router.get('/', function(req, res){
   });
 });
 
-router.get('/about', function(req, res){
+router.get('/about', function (req, res) {
   res.render('about', {
     title: 'About'
   });
 });
 
-router.get('/contact', function(req, res){
+router.get('/contact', function (req, res) {
   res.render('contact', {
     title: 'Contact'
   });
 });
 
-router.post('/login', function(req, res){
+router.post('/login', function (req, res) {
   var passCode = req.body.passcode;
-  
+
   futService.requestLogin(passCode);
 
-  res.render('index', { 
-    title: 'Home' 
+  res.render('index', {
+    title: 'Home'
   });
 });
 
 
-router.post('/getCredits', function(req, res){
-  
-  futService.getCredits(function(credits){
-      res.render('index', { 
-        title: 'Home',
-        credits: credits
-      });
+router.post('/getCredits', function (req, res) {
+
+  futService.getCredits(function (credits) {
+    res.render('index', {
+      title: 'Home',
+      credits: credits
+    });
   });
 });
 
-router.post('/getPlayer', function(req, res){  
-  var data = { 
+router.post('/getPlayer', function (req, res) {
+  var data = {
     maxbuy: req.body.maxbuy,
     level: req.body.level,
     minprice: req.body.minprice,
     maxprice: req.body.maxprice,
     league: req.body.league,
     playerid: req.body.playerid,
-    position: req.body.position
+    position: req.body.position,
+    isRare: req.body.isRare ? 'CR' : ''
   };
-  
-  futService.findplayer(data, function(){
-      res.render('index', { 
-        title: 'Home',
-        maxbuy: req.body.maxbuy
-      });
+
+  futService.findplayer(data, function () {
+    res.render('index', {
+      title: 'Home',
+      maxbuy: req.body.maxbuy
+    });
   });
 });
 
-router.post('/getWatchList', function(req, res){  
-  futService.getWatchList(function(watchList){
-      res.render('index', { 
-        title: 'Home',
-        data: watchList
-      });
+router.post('/getWatchList', function (req, res) {
+  futService.getWatchList(function (watchList) {
+    res.render('index', {
+      title: 'Home',
+      data: watchList
+    });
+  });
+});
+
+router.post('/processcriteria', function (req, res) {
+
+  var data = {
+    maxbuy: req.body.maxbuy,
+    level: req.body.level,
+    minprice: req.body.minprice,
+    maxprice: req.body.maxprice,
+    league: req.body.league,
+    playerid: req.body.playerid,
+    position: req.body.position,
+    isRare: req.body.isRare ? 'CR' : ''
+  };
+
+  futService.processcriteria(data);
+
+  res.render('index', {
+    title: 'Home'
   });
 });
 
